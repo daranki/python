5558201005943462040d5d8f6acb63bec55240c9
daranki <Dylan.issa.aranki@gmail.com>
2025-04-24 09:27:13 -0500

    starter code for unit test file inserted

--- python/test_television.py
+++ python/test_television.py
@@ -1,1 +1,44 @@
-print('this is the unit test file 1')
+import pytest
+from television import *
+
+class Test:
+    def setup_method(self):
+        self.tvl = Television()
+
+    def teardown_method(self):
+        del self.tvl
+
+    def test_init(self):
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_power(self):
+        self.tvl.power()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        self.tvl.power()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_mute(self):
+        pass
+
+    def test_channel_up(self):
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+        self.tvl.power()
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = True, Channel = 1, Volume = 0'
+
+        self.tvl.channel_up()
+        self.tvl.channel_up()
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+    def test_channel_down(self):
+        pass
+
+    def test_volume_up(self):
+        pass
+
+    def test_volume_down(self):
+        pass
