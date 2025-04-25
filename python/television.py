71bdc5c8a6c70f5eeb2e6c786897c961919277f0
daranki <Dylan.issa.aranki@gmail.com>
2025-04-24 19:56:57 -0500

    completed but not unit tested television code (master)

--- python/television.py
+++ python/television.py
@@ -11,19 +11,26 @@ class Television:
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        pass
+        self.__status = not self.__status
 
     def mute(self):
-        pass
+        if self.__status:
+            self.__muted = not self.__muted
 
     def channel_up(self):
-        pass
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
         if self.__status:
-            self.__muted = False
-            if self.__volume < Television.MAX_VOLUME:
-                self.__volume += 1
+            if self.__channel > Television.MIN_CHANNEL:
+                 self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
 
     def volume_up(self):
         if self.__status:
@@ -32,10 +39,13 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self):
-        pass
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
 
     def __str__(self):
         if self.__muted:
-            return f'Volume = {Television.MIN_VOLUME}'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
-            return f'xxx'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
